ae536cffd4c5d6f9989dd0f01693e7b89d5d4376
Italogabrieldias <italogabrielde@gmail.com>
2024-04-01 21:58:51 -0300

    Move games on list

--- src/main/java/com/flametec/listgamepro/services/GameListService.java
+++ src/main/java/com/flametec/listgamepro/services/GameListService.java
@@ -2,7 +2,9 @@ package com.flametec.listgamepro.services;
 
 import com.flametec.listgamepro.DTO.GameListDTO;
 import com.flametec.listgamepro.entities.GameList;
+import com.flametec.listgamepro.projections.GameMinProjection;
 import com.flametec.listgamepro.repositories.GameListRepository;
+import com.flametec.listgamepro.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,10 +15,27 @@ public class GameListService {
 
     @Autowired
     private GameListRepository gameListRepository;
+
+    @Autowired
+    GameRepository gameRepository;
     @Transactional(readOnly = true)
     public List<GameListDTO> findAll(){
         List<GameList> result = gameListRepository.findAll();
         return result.stream().map(x -> new GameListDTO(x)).toList();
 
     }
+    @Transactional(readOnly = true)
+    public void move(Long ListId, int sourceIndex, int destinationIndex){
+        List<GameMinProjection> list = gameRepository.searchByList(ListId);
+        GameMinProjection obj = list.remove(sourceIndex);
+        list.add(destinationIndex, obj);
+        int min = sourceIndex < destinationIndex ? sourceIndex : destinationIndex;
+        int max = sourceIndex < destinationIndex ? destinationIndex : sourceIndex;
+
+        for (int i = min; i<=max;i++){
+            gameListRepository.updateBelongingPosition(ListId, list.get(i).getId(),i);
+
+        }
+        }
+
 }
